63eeca8f37295ae62f15728da5e41744f8deaa9a
Julia <129061308+JuliaStavychna@users.noreply.github.com>
2023-10-09 17:20:38 +0300

    fixing errors

--- src/index.js
+++ src/index.js
@@ -18,4 +18,111 @@ const lightbox = new SimpleLightbox('.lightbox', {
   scrollZoom: false,
   close: false,
 });
+searchForm.addEventListener('submit', onFormSybmit);
+window.addEventListener('scroll', onScrollHandler);
+document.addEventListener('DOMContentLoaded', hideLoader);
 
+function showLoader() {
+  loaderEl.style.display = 'block';
+}
+
+function hideLoader() {
+  loaderEl.style.display = 'none';
+}
+
+function renderGallery(hits) {
+  const markup = hits
+    .map(item => {
+      return `
+            <a href="${item.largeImageURL}" class="lightbox">
+                <div class="photo-card">
+                    <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>
+                            ${item.likes}
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>
+                            ${item.views}
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>
+                            ${item.comments}
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>
+                            ${item.downloads}
+                        </p>
+                    </div>
+                </div>
+            </a>
+            `;
+    })
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+
+  if (options.params.page * options.params.per_page >= totalHits) {
+    if (!reachedEnd) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      reachedEnd = true;
+    }
+  }
+  lightbox.refresh();
+}
+
+async function fetchDataFromApi() {
+  try {
+    showLoader();
+    const response = await axios.get(BASE_URL, options);
+    return response.data.hits;
+  } catch (err) {
+    Notify.failure(err);
+    throw err;
+  } finally {
+    hideLoader();
+  }
+}
+
+function onScrollHandler() {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  const scrollThreshold = 300;
+  if (
+    scrollTop + clientHeight >= scrollHeight - scrollThreshold &&
+    galleryEl.innerHTML !== '' &&
+    !isLoadingMore &&
+    !reachedEnd
+  ) {
+    loadMore();
+  }
+}
+
+async function onFormSybmit(e) {
+  e.preventDefault();
+  options.params.q = searchInput.value.trim();
+  if (options.params.q === '') {
+    return;
+  }
+  options.params.page = 1;
+  galleryEl.innerHTML = '';
+  reachedEnd = false;
+
+  try {
+    const hits = await fetchDataFromApi();
+    totalHits = hits.length;
+    if (totalHits === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+      renderGallery(hits);
+    }
+    searchInput.value = '';
+    hideLoader();
+  } catch (err) {
+    Notify.failure(err);
+    hideLoader();
+  }
+}
